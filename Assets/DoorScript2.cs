ec2b6c89721675a56a8db58467c2181786490253
Queenjewls <julialic2230@gmail.com>
2024-05-01 03:45:12 +0100

    BIG UPDATE
    3 interactions etc

---
+++ Assets/DoorScript2.cs
@@ -0,0 +1,30 @@
+
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class DoorScript2 : MonoBehaviour
+{
+    public Transform doorTransform;       // Assign the actual door transform that should move
+    public Vector3 openPosition;          // Manually set this in the Inspector to the desired open position
+    public float doorOpenSpeed = 5f;      // Speed at which the door opens
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the colliding object is the key
+        if (collision.gameObject.CompareTag("Key"))
+        {
+            StartCoroutine(OpenDoor());
+        }
+    }
+
+    private IEnumerator OpenDoor()
+    {
+        while (Vector3.Distance(doorTransform.position, openPosition) > 0.01f) // Added a small threshold to ensure smooth stopping
+        {
+            doorTransform.position = Vector3.MoveTowards(doorTransform.position, openPosition, Time.deltaTime * doorOpenSpeed);
+            yield return null;
+        }
+    }
+}
+
---
+++ Assets/Throwable.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwable : MonoBehaviour
+
+{
+    // Reference to the door GameObject
+    public GameObject door;
+
+    // Number of targets to hit
+    public int numberOfTargets = 3;
+
+    // Counter for the number of targets hit
+    private int targetsHit = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if all targets have been hit
+        if (targetsHit >= numberOfTargets)
+        {
+            OpenDoor();
+        }
+    }
+
+    // Method to open the door
+    void OpenDoor()
+    {
+        // Implement your door opening logic here
+        Debug.Log("Opening door...");
+        // Example: door.GetComponent<Animator>().SetTrigger("Open");
+    }
+
+    // Method called when a sword hits a target
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the collider belongs to a sword
+        if (other.CompareTag("Sword"))
+        {
+            // Increment the targetsHit counter
+            targetsHit++;
+            // Optionally, you can disable the sword to prevent it from hitting other targets
+            other.gameObject.SetActive(false);
+        }
+    }
+}
+
+
